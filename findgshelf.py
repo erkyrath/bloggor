e65c638240d8c62da669f405e25c3cdbbf04cf10
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-15 13:43:48 -0400

    Misc scripts.

---
+++ findgshelf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+pat_gameshelf = re.compile('["\'](http[s]?://[a-z0-9_.]*jmac.org/[^"\']*)["\']', flags=re.IGNORECASE)
+
+allurls = set()
+
+def check(filename):
+    fl = open(filename)
+    dat = fl.read()
+    fl.close()
+
+    ls = pat_gameshelf.findall(dat)
+    for url in ls:
+        allurls.add(url)
+
+for filename in sys.argv[1:]:
+    check(filename)
+    
+ls = list(allurls)
+ls.sort()
+for url in ls:
+    print(url)
