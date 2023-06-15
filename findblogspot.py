bada63bb5cf67a683fe8dfb5e37042f975f8d60f
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-15 15:23:43 -0400

    More temp scripts.

---
+++ findblogspot.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+pat_gameshelf = re.compile('["\'](http[s]?://[a-z0-9_.]*(?:googleusercontent|blogspot).com/[^"\']*)["\']', flags=re.IGNORECASE)
+
+allurls = set()
+
+def check(filename):
+    fl = open(filename)
+    dat = fl.read()
+    fl.close()
+
+    ls = pat_gameshelf.findall(dat)
+    for url in ls:
+        allurls.add(url)
+
+for filename in sys.argv[1:]:
+    check(filename)
+    
+ls = list(allurls)
+ls.sort()
+for url in ls:
+    if url.endswith('.html') or url.endswith('.com/'):
+        continue
+    print(url)
