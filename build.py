ae36801af69d4e8a9570a5bb2721d1dc7e909ddc
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-16 23:51:29 -0400

    Start walking the entries.

--- build.py
+++ build.py
@@ -7,9 +7,12 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 popt = optparse.OptionParser()
 
+popt.add_option('--src',
+                action='store', dest='srcdir', default='.',
+                help='source directory')
 popt.add_option('-o', '--out',
-                action='store', dest='sitedir', default='site',
-                help='site directory')
+                action='store', dest='destdir', default='site',
+                help='destination directory')
 
 (opts, args) = popt.parse_args()
 
@@ -19,7 +22,14 @@ jenv = Environment(
     keep_trailing_newline = True,
 )
 
-mdenv = markdown.Markdown()
+for dirpath, dirnames, filenames in os.walk(os.path.join(opts.srcdir, 'entries')):
+    for filename in filenames:
+        if filename.endswith('~'):
+            continue
+        print(dirpath, filename)
+    
+
+mdenv = markdown.Markdown(extensions=['meta', 'def_list', 'fenced_code', 'tables'])
 
 fl = open('pages/home.md')
 dat = fl.read()
@@ -27,7 +37,7 @@ fl.close()
 mdenv.reset()
 body = mdenv.convert(dat)
 
-fl = open(os.path.join(opts.sitedir, 'index.html'), 'w')
+fl = open(os.path.join(opts.destdir, 'index.html'), 'w')
 template = jenv.get_template('page.html')
 fl.write(template.render(title='Zarf Updates', body=body))
 fl.close()
