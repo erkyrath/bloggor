1ad92b41b062d2b8e583c22da6415eef6506903d
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-18 13:57:31 -0400

    Utility to create a filename for an arbitrary tag.

---
+++ bloggor/util.py
@@ -0,0 +1,16 @@
+import re
+
+pat_basictag = re.compile('^[a-z0-9 -]*$')
+pat_fancychar = re.compile('[^a-z0-9-]')
+
+def escapefancy(match):
+    ch = match.group(0)
+    if ch == ' ':
+        return '_'
+    return '_%X_' % (ord(ch),)
+
+def tagfilename(val):
+    if pat_basictag.match(val):
+        return val.replace(' ', '_')
+    return pat_fancychar.sub(escapefancy, val)
+    
