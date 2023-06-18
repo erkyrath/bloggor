f4d6a41781a9bac23d76e59dc1d7cd4e336dd047
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-18 11:31:48 -0400

    Do the menu thing.

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,9 +1,18 @@
 'use strict';
 
+var menuopen = false;
+
 function handle_menu_button(ev)
 {
     ev.stopPropagation();
     ev.preventDefault();
-    console.log('###', ev);
+    if (!menuopen) {
+	menuopen = true;
+	document.body.classList.add('MenuOpen');
+    }
+    else {
+	menuopen = false;
+	document.body.classList.remove('MenuOpen');
+    }
     return true;
 }
