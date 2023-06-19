d1eddf185da6e4feeb23fcfcdc7db7b19e1e871c
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-19 18:51:32 -0400

    Experimental versifier.

--- bloggor/mdextension.py
+++ bloggor/mdextension.py
@@ -38,3 +38,35 @@ class MoreBreakExtension(Extension):
         md.parser.blockprocessors.register(MoreBreakProcessor(md.parser), 'morebreak', 100)
         md.postprocessors.register(MorePostProcessor(md.parser), 'morepost', 0)
 
+
+class BoxBlockProcessor(BlockProcessor):
+    RE_FENCE_START = r'^ *!{3,} *\n' # start line, e.g., `   !!!! `
+    RE_FENCE_END = r'\n *!{3,}\s*$'  # last non-blank line, e.g, '!!!\n  \n\n'
+
+    def test(self, parent, block):
+        return re.match(self.RE_FENCE_START, block)
+
+    def run(self, parent, blocks):
+        original_block = blocks[0]
+        blocks[0] = re.sub(self.RE_FENCE_START, '', blocks[0])
+
+        # Find block with ending fence
+        for block_num, block in enumerate(blocks):
+            if re.search(self.RE_FENCE_END, block):
+                # remove fence
+                blocks[block_num] = re.sub(self.RE_FENCE_END, '', block)
+                # render fenced area inside a new div
+                e = etree.SubElement(parent, 'div')
+                e.set('class', 'PreWrap')
+                self.parser.parseBlocks(e, blocks[0:block_num + 1])
+                # remove used blocks
+                for i in range(0, block_num + 1):
+                    blocks.pop(0)
+                return True
+        # No closing marker!  Restore and do nothing
+        blocks[0] = original_block
+        return False  # equivalent to our test() routine returning False
+
+class BoxExtension(Extension):
+    def extendMarkdown(self, md):
+        md.parser.blockprocessors.register(BoxBlockProcessor(md.parser), 'box', 175)
