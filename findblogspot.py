65b62c7ed12c23537999eb1104fc7cc6fc368007
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-20 14:24:44 -0400

    Add prefix, hostpost to blogger-image-urls.

--- findblogspot.py
+++ findblogspot.py
@@ -2,26 +2,40 @@
 
 import sys
 import re
+import os.path
+
+from readtable import read_table
+from bloggor.util import MultiDict
+
+post_name_table = read_table('post-name-table', multi=True)
 
 pat_blogspot = re.compile('["\'](http[s]?://[a-z0-9_.]*(?:googleusercontent|blogspot).com/[^"\']*)["\']', flags=re.IGNORECASE)
 
-allurls = set()
+allurls = MultiDict()
 
 def check(filename):
+    shortid = os.path.basename(filename).replace('.html', '')
+    prefix, parentpost = post_name_table[shortid]
+
     fl = open(filename)
     dat = fl.read()
     fl.close()
 
     ls = pat_blogspot.findall(dat)
     for url in ls:
-        allurls.add(url)
+        allurls.add(url, (prefix, parentpost))
 
 for filename in sys.argv[1:]:
     check(filename)
     
-ls = list(allurls)
+ls = list(allurls.keys())
 ls.sort()
 for url in ls:
     if url.endswith('.html') or url.endswith('.com/'):
         continue
+    prefixls = allurls[url]
+    prefixls.sort()
+    prefix, parentpost = prefixls[0]
     print(url)
+    print('  '+prefix)
+    print('  '+parentpost)
