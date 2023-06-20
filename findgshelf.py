6ef3fa228f76da63c70ae7e710af7c529b6f73fc
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-20 12:46:55 -0400

    gameshelf-urls now contains post prefixes and filenames where the URLs appear.

--- findgshelf.py
+++ findgshelf.py
@@ -2,24 +2,39 @@
 
 import sys
 import re
+import os.path
+
+from readtable import read_table
+from bloggor.util import MultiDict
+
+post_name_table = read_table('post-name-table', multi=True)
 
 pat_gameshelf = re.compile('["\'](http[s]?://[a-z0-9_.]*jmac.org/[^"\']*)["\']', flags=re.IGNORECASE)
 
-allurls = set()
+allurls = MultiDict()
 
 def check(filename):
+    shortid = os.path.basename(filename).replace('.html', '')
+    prefix, parentpost = post_name_table[shortid]
+
     fl = open(filename)
     dat = fl.read()
     fl.close()
 
     ls = pat_gameshelf.findall(dat)
     for url in ls:
-        allurls.add(url)
+        allurls.add(url, (prefix, parentpost))
 
 for filename in sys.argv[1:]:
     check(filename)
     
-ls = list(allurls)
+ls = list(allurls.keys())
 ls.sort()
 for url in ls:
+    prefixls = allurls[url]
+    prefixls.sort()
+    prefix, parentpost = prefixls[0]
     print(url)
+    print('  '+prefix)
+    print('  '+parentpost)
+    
