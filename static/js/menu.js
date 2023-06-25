db226749df05eb3e4f44fb9ac8df84fc91ccdf44
Andrew Plotkin <zarf@ZarfLent.local>
2023-06-25 16:03:08 -0400

    Handle the menu stuff dynamically.

--- static/js/menu.js
+++ static/js/menu.js
@@ -8,11 +8,19 @@ function handle_menu_button(ev)
     ev.preventDefault();
     if (!menuopen) {
         menuopen = true;
+        var nod = document.getElementById('menucolumn');
+        nod.textContent = '';
+        var newnod = document.getElementById('sidebar').cloneNode(true);
+        newnod.id = 'sidebarclone';
+        newnod.className = 'MenuContent Sidebar';
+        nod.insertBefore(newnod, null);
         document.body.classList.add('MenuOpen');
     }
     else {
         menuopen = false;
         document.body.classList.remove('MenuOpen');
+        var nod = document.getElementById('menucolumn');
+        nod.textContent = '';
     }
     return true;
 }
