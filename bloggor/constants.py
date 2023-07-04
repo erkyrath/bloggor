b3992136ce9314f02b50f2a934d3eb68c36e5618
Andrew Plotkin <zarf@ZarfLent.local>
2023-07-04 19:35:34 -0400

    parse_depend().

--- bloggor/constants.py
+++ bloggor/constants.py
@@ -8,15 +8,17 @@ class FileType(Enum):
     TXT = 'txt'
 
 def parse_filetype(val):
+    val = val.lower()
     if val == 'text':
         return FileType.TXT
-    return FileType(val.lower())
+    return FileType(val)
     
 class FeedType(Enum):
     RSS = 'rss'
     ATOM = 'atom'
 
 class Depend(IntFlag):
+    NONE     = 0
     CREATED  = 1
     TITLE    = 2
     BODY     = 4
@@ -27,5 +29,25 @@ class Depend(IntFlag):
 
     ALL = CREATED|TITLE|BODY|TAGS|PUBDATE|UPDATE|COMMENTS
     ALLBUTBODY = CREATED|TITLE|TAGS|PUBDATE|UPDATE|COMMENTS
+
+def parse_depend(val):
+    if ',' in val:
+        res = Depend.NONE
+        for subval in val.split(','):
+            res |= parse_depend(subval)
+        return res
+    val = val.upper()
+    if val == 'TAG':
+        return Depend.TAGS
+    if val == 'COMMENT':
+        return Depend.COMMENTS
+    if val == 'ALL':
+        return Depend.ALL
+    if val == 'NONE':
+        return Depend.NONE
+    for dep in Depend:
+        if dep.name == val:
+            return dep
+    raise ValueError(val)
     
 eastern_tz = pytz.timezone('US/Eastern')
